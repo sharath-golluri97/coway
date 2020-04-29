b67801f0aaf5fff6e37cce3d90b4ae50082ad2b5
sharath.golluri <sharath.golluri@inmobi.com>
2020-04-30 02:33:09 +0530

    Root page configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import AppBar from './client/commons/appBar/appBar.component';
 import './App.css';
 
 function App() {
   return (
     <div>
-      Welcome to Coway!
-       {/*Add landing page component here  */}
+	    <ThemeProvider>
+		    <AppBar/>
+	    </ThemeProvider>
+      {/* Welcome to Coway! */}
+      {/*Add landing page component here  */}
     </div>
   );
 }
