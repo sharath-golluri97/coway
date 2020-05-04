fc91454927ef7386f711ad2c52f3caad1aa65ef2
Aman Gupta <amangupta570@gmail.com>
2020-05-04 22:08:53 +0530

    Temporary Chat Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import AppBar from './client/commons/appBar/appBar.component';
 import './App.css';
-import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
 import ScrollTop from "./client/commons/appBar/scrollTop.molecule";
 import Fab from "@material-ui/core/Fab";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import ChatRoom from "./client/components/chatRoom/chatRoom.component";
+import ChatRoomsList from "./client/components/chatRoom/chatRoomsList.component";
 
 
 function App() {
@@ -14,14 +13,8 @@ function App() {
     <div>
 	    {/*<ThemeProvider>*/}
 		    <AppBar/>
-            <Container>
-                <Box my={2}>
-
                 {/*  Include all the routes   */}
-
-
-                </Box>
-            </Container>
+                <ChatRoomsList/>
             <ScrollTop>
                 <Fab color="secondary" size="small" aria-label="scroll back to top">
                     <KeyboardArrowUpIcon />
