de1be04a98bfbc0cf4a5450ec293c932fdc2bb5e
Aman Gupta <amangupta570@gmail.com>
2020-05-04 22:19:22 +0530

    Azure SignalR Send Message Binding

---
+++ src/server/chatAppServer/SendMessage/index.js
@@ -0,0 +1,20 @@
+module.exports = function (context, req) {
+    const message = req.body;
+    if (req.headers && req.headers['x-ms-client-principal-name']) {
+        message.sender = req.headers['x-ms-client-principal-name'];
+    }
+
+    let recipientUserId = '';
+    if (message.recipient) {
+        recipientUserId = message.recipient;
+        message.isPrivate = true;
+    }
+
+    context.bindings.signalRMessages = [{
+        "userId": recipientUserId,
+        "target": "newMessage",
+        "arguments": [ message ]
+    }];
+   context.res.body = message;
+    context.done();
+};
