34a3aa518d98b33e5248a25d7e84f29442d89898
Aman Gupta <amangupta570@gmail.com>
2020-05-16 05:51:26 +0530

    Added custom theme object

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+    primary: { main: '#311B92' },
+    secondary: { main: '#E040FB' }
+};
+const themeName = 'Jacksons Purple Heliotrope Cardinal';
+
+export default createMuiTheme({ palette, themeName });
